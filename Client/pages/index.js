9cb1cd368808df53d1cb2f04a55b4a234704c4a0
Karthik <karthikalakunta21@gmail.com>
2022-12-18 14:35:56 +0530

    added

--- Client/pages/index.js
+++ Client/pages/index.js
@@ -1,14 +1,13 @@
 // import Head from "next/head";
-// import { useEffect } from "react";
 import jwt from "jsonwebtoken";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 // import styles from "../styles/Home/Home.module.css";
-export default function Home({handleAuth,token}) {
+export default function Home({handleAuth}) {
   const router = useRouter();
+  let {token} = router.query; 
   useEffect(() =>{
     if(token) localStorage.setItem("token",JSON.stringify(token))
-
     let toke = JSON.parse(localStorage.getItem("token"))||"";
     if (toke.length > 1) {
       const details = jwt.decode(toke);
@@ -17,17 +16,10 @@ export default function Home({handleAuth,token}) {
        
     }
     handleAuth();
-  } , []);
+  } , [token]);
   return (
     <div>
       <h1>Index File</h1>
     </div>
   );
-}
-export function getServerSideProps({req,res}){
-  return {
-    props:{
-      token:req.cookies.TOKEN|| ""
-    }
-  }
 }
